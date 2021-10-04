f7ec558573c876c27bbb3769e01bc86b83a2ba7b
GustavoAns <gugasato9@live.com>
2021-10-04 02:11:05 -0300

    Ex 3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderComRotas from './renderComRotas';
+import { FavoritePokemons } from '../components';
+
+describe('Testa o componente FavoritePokemons', () => {
+  test('Testa se renderiza a mensagem no favorites', () => {
+    renderComRotas(<FavoritePokemons pokemons={ [] } />);
+
+    const noFavorite = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  test('Testa se renderiza os cards', () => {
+    const listaFavoritas = [
+      {
+        id: 25,
+        name: 'Pikachu',
+        type: 'Electric',
+        averageWeight: {
+          value: '6.0',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      },
+      {
+        id: 4,
+        name: 'Charmander',
+        type: 'Fire',
+        averageWeight: {
+          value: '8.5',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+      },
+    ];
+    renderComRotas(<FavoritePokemons pokemons={ listaFavoritas } />);
+
+    const favoritados = screen.getAllByTestId('pokemon-name');
+    expect(favoritados).toHaveLength(2);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderComRotas from './renderComRotas';
+import { NotFound } from '../components';
+
+describe('Testa o componente NotFound', () => {
+  test('Teste se página contém um heading h2 com Page requested not found', () => {
+    renderComRotas(<NotFound />);
+
+    const notFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      nivel: 2,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    renderComRotas(<NotFound />);
+
+    const imgNotFound = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
